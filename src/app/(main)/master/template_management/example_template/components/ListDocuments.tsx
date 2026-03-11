6fa80b3719341db194da1eded59214e45d879c44
lowhanfish <lowhanfish@gmail.com>
2026-03-12 06:58:48 +0800

    added Code Wraper

--- src/app/(main)/master/template_management/example_template/components/ListDocuments.tsx
+++ src/app/(main)/master/template_management/example_template/components/ListDocuments.tsx
@@ -1,20 +1,52 @@
-import React from 'react'
 import BlistDocument from '@/components/items/BlistDocument';
+import CodeWrapper from './CodeWrapper';
+
+
+const ListDocuments = () => {
+
+    const codeString = `
+import BlistDocument from '@/components/items/BlistDocument';
+import CodeWrapper from './CodeWrapper';
+
 
 const ListDocuments = () => {
     return (
-        <div className='flex bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
+        <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
             <div className='grid w-full'>
                 <div className='grid grid-cols-12 gap-2 md:gap-2'>
                     {
                         [...Array(4)].map((_, index) => (
-
                             <div key={index} className='col-span-12 md:col-span-6 w-full'>
                                 <BlistDocument />
                             </div>
                         ))
                     }
                 </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListDocuments
+
+        `;
+
+    return (
+        <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
+            <div className='grid w-full'>
+                <div className='grid grid-cols-12 gap-2 md:gap-2'>
+                    {
+                        [...Array(4)].map((_, index) => (
+                            <div key={index} className='col-span-12 md:col-span-6 w-full'>
+                                <BlistDocument />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <CodeWrapper codeString={codeString} />
             </div>
         </div>
     )
--- src/app/(main)/master/template_management/example_template/components/ListUser.tsx
+++ src/app/(main)/master/template_management/example_template/components/ListUser.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import BlistUser from '@/components/items/BlistUser';
+import CodeWrapper from './CodeWrapper';
+
+
+const ListUser = () => {
+
+    const codeString = `
+import React from 'react'
+import BlistUser from '@/components/items/BlistUser';
 
 const ListUser = () => {
     return (
@@ -8,17 +16,39 @@ const ListUser = () => {
                 <div className='grid grid-cols-12 gap-2 md:gap-4'>
                     {
                         [...Array(4)].map((_, index) => (
-
                             <div key={index} className='col-span-12 md:col-span-3 w-full'>
-
                                 <BlistUser />
-
                             </div>
-
-
                         ))
                     }
                 </div>
+            </div>
+        </div>
+    )
+}
+
+export default ListUser
+   
+    
+    `
+
+
+    return (
+        <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
+            <div className='grid w-full'>
+                <div className='grid grid-cols-12 gap-2 md:gap-4'>
+                    {
+                        [...Array(4)].map((_, index) => (
+                            <div key={index} className='col-span-12 md:col-span-3 w-full'>
+                                <BlistUser />
+                            </div>
+                        ))
+                    }
+                </div>
+            </div>
+
+            <div className='mt-5'>
+                <CodeWrapper codeString={codeString} />
             </div>
         </div>
     )
