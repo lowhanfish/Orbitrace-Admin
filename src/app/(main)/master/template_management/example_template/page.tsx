93bba161be1995df043a912734cf2e755aa1dc5e
lowhanfish <lowhanfish@gmail.com>
2026-03-12 00:41:45 +0800

    added TitleBar

--- src/app/(main)/master/template_management/example_template/page.tsx
+++ src/app/(main)/master/template_management/example_template/page.tsx
@@ -1,5 +1,6 @@
 import BInput from '@/components/items/BInput'
 import TitleBar from '@/components/TitleBar'
+import { BsFileTextFill } from "react-icons/bs";
 import React from 'react'
 
 
@@ -32,12 +33,31 @@ const page = () => {
 
             <div className='flex bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
 
-                <div className='grid'>
-                    <div className='grid grid-cols-12'>
-                        <div className='col-span-4'>
+                <div className='grid w-full'>
+                    <div className='grid grid-cols-12 gap-5'>
 
 
-                        </div>
+                        {
+                            [...Array(10)].map(() => (
+
+                                <div className='col-span-6 w-full'>
+                                    <div className='flex w-full bg-b-gray-2/70 rounded-[3]'>
+                                        <div className='bg-red-300 flex items-center'>
+                                            <BsFileTextFill />
+                                        </div>
+                                        <div className='flex-1 bg-red-400'>
+                                            <p>Title</p>
+                                        </div>
+                                        <div className='bg-red-500'>
+                                            :
+                                        </div>
+                                    </div>
+
+                                </div>
+                            ))
+                        }
+
+
                     </div>
                 </div>
 
