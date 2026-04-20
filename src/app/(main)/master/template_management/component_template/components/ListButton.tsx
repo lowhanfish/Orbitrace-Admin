f79317dcd7d4f8a9a9ee31291a043fbc28662ee1
lowhanfish <lowhanfish@gmail.com>
2026-04-20 22:43:08 +0800

    added button

--- src/app/(main)/master/template_management/component_template/components/ListButton.tsx
+++ src/app/(main)/master/template_management/component_template/components/ListButton.tsx
@@ -9,10 +9,75 @@ const ListButton = () => {
         <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
             <div className='grid grid-cols-1 md:grid-cols-12 gap-x-5 gap-y-1 w-full'>
 
-                <div className='col-span-3 '>
-                    {/* <BButton>
-                        <p>asdad</p>
-                    </BButton> */}
+                <div className='col-span-6 flex flex-row  gap-2'>
+
+                    <BButton
+                        color='gray'
+                        size='xs'
+                    >
+                        <p className='text-b-gray-6 text-[12px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='md'
+                    >
+                        <p className='text-b-gray-6 text-[14px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='lg'
+                    >
+                        <p className='text-b-gray-6 text-[15px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='xl'
+                    >
+                        <p className='text-b-gray-6 text-[16px]'>Gray</p>
+                    </BButton>
+
+                </div>
+
+                <div className='col-span-6 flex flex-row  gap-2'>
+
+                    <BButton
+                        color='gray'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='blue'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Blue</p>
+                    </BButton>
+                    <BButton
+                        color='green'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Green</p>
+                    </BButton>
+                    <BButton
+                        color='yellow'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Yellow</p>
+                    </BButton>
+                    <BButton
+                        color='red'
+                        size='sm'
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Red</p>
+                    </BButton>
+
+
                 </div>
 
 
--- src/components/items/BButton.tsx
+++ src/components/items/BButton.tsx
@@ -3,28 +3,38 @@ import { ReactNode } from 'react'
 
 interface BButtonProps {
     color: string,
-    children: ReactNode
+    children: ReactNode,
+    size: string
 }
 
 const colorMap: Record<string, string> = {
-    gray: "bg-b-red-3 hover:bg-b-red-4 text-b-gray-1",
-    blue: "bg-b-red-3 hover:bg-b-red-4 text-b-gray-1",
-    green: "bg-b-red-3 hover:bg-b-red-4 text-b-gray-1",
-    yellow: "bg-b-red-3 hover:bg-b-red-4 text-b-gray-1",
-    red: "bg-b-red-3 hover:bg-b-red-4 text-b-gray-1",
+    gray: "bg-gradient-to-r from-b-gray-5/30 via-b-gray-3 to-b-gray-5/50 text-b-gray-6 hover:from-b-gray-4/70 hover:to-b-gray-4/50",
+    blue: "bg-gradient-to-r from-b-blue-5 via-b-blue-6 to-b-blue-5 text-b-blue-6 hover:from-b-blue-4 hover:to-b-blue-4",
+    green: "bg-gradient-to-r from-b-green-5 via-b-green-6 to-b-green-5 text-b-green-6 hover:from-b-green-4 hover:to-b-green-4",
+    yellow: "bg-gradient-to-r from-b-yellow-4/90 via-b-yellow-5 to-b-yellow-4/90 text-b-yellow-6 hover:from-b-yellow-2 hover:to-b-yellow-2",
+    red: "bg-gradient-to-r from-b-red-3/90 via-b-red-5 to-b-red-3/90 text-b-red-6 hover:from-b-red-2 hover:to-b-red-2",
+}
+
+const sizeMap: Record<string, string> = {
+    xs: 'h-5',
+    sm: 'h-7',
+    md: 'h-9',
+    lg: 'h-11',
+    xl: 'h-13',
 }
 
 
 
-const BButton = ({ color, children }: BButtonProps) => {
+const BButton = ({ color, children, size }: BButtonProps) => {
     return (
-        <div>
+        <div className='w-full'>
             <div
                 className={`
                 flex flex-row gap-2 justify-center items-center
                 ${colorMap[color]}
                 shadow-lg
-                h-10 w-full
+                ${sizeMap[size]}
+                w-full
                 rounded-sm
                 cursor-pointer
             `}>
