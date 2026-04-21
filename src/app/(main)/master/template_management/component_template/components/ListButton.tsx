70435fab7493993e623b989f39975cea633603eb
lowhanfish <lowhanfish@gmail.com>
2026-04-21 13:02:30 +0800

    added button overlay

--- src/app/(main)/master/template_management/component_template/components/ListButton.tsx
+++ src/app/(main)/master/template_management/component_template/components/ListButton.tsx
@@ -1,88 +1,157 @@
+"use client"
 import React from 'react'
 import CodeWrapper from './CodeWrapper'
 import BButton from '@/components/items/BButton'
 
-
-
 const ListButton = () => {
+
+    const testClick = () => {
+        console.log("hy")
+    }
+
     return (
         <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
-            <div className='grid grid-cols-1 md:grid-cols-12 gap-x-5 gap-y-1 w-full'>
+            <div className='grid grid-cols-1 md:grid-cols-12 gap-x-5 gap-y-10 w-full'>
 
-                <div className='col-span-6 flex flex-row  gap-2'>
-
+                <div className='col-span-6 flex flex-col md:flex-row  gap-2'>
                     <BButton
                         color='gray'
                         size='xs'
+                        onClick={() => testClick()}
                     >
-                        <p className='text-b-gray-6 text-[12px]'>Gray</p>
+                        <p className='text-b-gray-6 text-[12px]'>size = xs</p>
                     </BButton>
                     <BButton
                         color='gray'
                         size='sm'
+                        onClick={() => testClick()}
                     >
-                        <p className='text-b-gray-6 text-[13px]'>Gray</p>
+                        <p className='text-b-gray-6 text-[13px]'>size = sm</p>
                     </BButton>
                     <BButton
                         color='gray'
                         size='md'
+                        onClick={() => testClick()}
                     >
-                        <p className='text-b-gray-6 text-[14px]'>Gray</p>
+                        <p className='text-b-gray-6 text-[14px]'>size = md</p>
                     </BButton>
                     <BButton
                         color='gray'
                         size='lg'
+                        onClick={() => testClick()}
                     >
-                        <p className='text-b-gray-6 text-[15px]'>Gray</p>
+                        <p className='text-b-gray-6 text-[15px]'>size = lg</p>
                     </BButton>
                     <BButton
                         color='gray'
                         size='xl'
+                        onClick={() => testClick()}
                     >
-                        <p className='text-b-gray-6 text-[16px]'>Gray</p>
+                        <p className='text-b-gray-6 text-[16px]'>size = xs</p>
                     </BButton>
 
                 </div>
 
-                <div className='col-span-6 flex flex-row  gap-2'>
+                <div className='col-span-6 flex flex-col md:flex-row  gap-2'>
 
                     <BButton
                         color='gray'
                         size='sm'
+                        onClick={() => testClick()}
                     >
                         <p className='text-b-gray-6 text-[13px]'>Gray</p>
                     </BButton>
                     <BButton
                         color='blue'
                         size='sm'
+                        onClick={() => testClick()}
                     >
                         <p className='text-b-gray-6 text-[13px]'>Blue</p>
                     </BButton>
                     <BButton
                         color='green'
                         size='sm'
+                        onClick={() => testClick()}
                     >
                         <p className='text-b-gray-6 text-[13px]'>Green</p>
                     </BButton>
                     <BButton
                         color='yellow'
                         size='sm'
+                        onClick={() => testClick()}
                     >
                         <p className='text-b-gray-6 text-[13px]'>Yellow</p>
                     </BButton>
                     <BButton
                         color='red'
                         size='sm'
+                        onClick={() => testClick()}
                     >
                         <p className='text-b-gray-6 text-[13px]'>Red</p>
                     </BButton>
-
-
                 </div>
 
 
+                <div className='col-span-12 flex flex-col md:flex-row  gap-2'>
+                    <BButton
+                        mode="3d"
+                        color='green'
+                        size='md'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-white font-semibold text-[13px] text-shadow-xs text-shadow-zinc-500'>Mode : glossy</p>
+                    </BButton>
+
+                    <BButton
+                        mode="glossy"
+                        color='red'
+                        size='md'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-white font-semibold text-[13px] text-shadow-xs text-shadow-zinc-500'>Mode : glossy</p>
+                    </BButton>
+
+                    <BButton
+                        mode="neo"
+                        color='red'
+                        size='md'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-7 font-semibold text-[13px] dark:text-shadow-xs dark:text-shadow-zinc-500'>Mode : neo</p>
+                    </BButton>
+                    <BButton
+                        mode="neon"
+                        color='red'
+                        size='md'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-white font-semibold text-[13px] text-shadow-xs text-shadow-zinc-500'>Mode : neon</p>
+                    </BButton>
 
 
+                    <button className="
+                        relative px-6 py-2 font-semibold text-white
+                        bg-linear-to-r from-purple-500 to-pink-500
+                        rounded-lg shadow-[0_0_20px_rgba(192,38,211,0.5)]
+                        hover:shadow-[0_0_30px_rgba(192,38,211,0.8)]
+                        hover:scale-105 transition-all duration-300
+                    ">
+                        Neon
+                    </button>
+
+
+                    <button className="
+                        px-6 py-2 font-medium border-2 border-blue-500 text-blue-500
+                        bg-transparent hover:bg-blue-500 hover:text-white
+                        transition-colors duration-300 rounded-md
+                    ">
+                        Secondary Action
+                    </button>
+
+
+
+
+                </div>
             </div>
 
             <div className='mt-5'>
@@ -99,17 +168,103 @@ export default ListButton
 
 const codeString = `
 "use client"
+import React from 'react'
+import BButton from '@/components/items/BButton'
 
-import { useState } from 'react'
-import BCheckBox from '@/components/items/BCheckBox'
+const ListButton = () => {
 
-const InputDataRd = () => {
+    const testClick = () => {
+        console.log("hy")
+    }
 
-    const [data, setData] = useState<boolean>(false);
+    return (
+        <div className='flex flex-col bg-linear-to-r from-b-gray-1 to-50% to-b-gray-1/40 shadow-sm rounded-[5] px-3 py-3 mt-2'>
+            <div className='grid grid-cols-1 md:grid-cols-12 gap-x-5 gap-y-10 w-full'>
 
-    
+                <div className='col-span-6 flex flex-col md:flex-row  gap-2'>
+                    <BButton
+                        color='gray'
+                        size='xs'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[12px]'>size = xs</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>size = sm</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='md'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[14px]'>size = md</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='lg'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[15px]'>size = lg</p>
+                    </BButton>
+                    <BButton
+                        color='gray'
+                        size='xl'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[16px]'>size = xs</p>
+                    </BButton>
+
+                </div>
+
+                <div className='col-span-6 flex flex-col md:flex-row  gap-2'>
+
+                    <BButton
+                        color='gray'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Gray</p>
+                    </BButton>
+                    <BButton
+                        color='blue'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Blue</p>
+                    </BButton>
+                    <BButton
+                        color='green'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Green</p>
+                    </BButton>
+                    <BButton
+                        color='yellow'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Yellow</p>
+                    </BButton>
+                    <BButton
+                        color='red'
+                        size='sm'
+                        onClick={() => testClick()}
+                    >
+                        <p className='text-b-gray-6 text-[13px]'>Red</p>
+                    </BButton>
+
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
-export default InputDataRd
+export default ListButton
     
 `
--- src/components/items/BButton.tsx
+++ src/components/items/BButton.tsx
@@ -1,18 +1,93 @@
+"use client"
 import { ReactNode } from 'react'
 
 
 interface BButtonProps {
     color: string,
     children: ReactNode,
-    size: string
+    size: string,
+    mode?: string,
+    onClick: () => void,
 }
 
 const colorMap: Record<string, string> = {
-    gray: "bg-gradient-to-r from-b-gray-5/30 via-b-gray-3 to-b-gray-5/50 text-b-gray-6 hover:from-b-gray-4/70 hover:to-b-gray-4/50",
-    blue: "bg-gradient-to-r from-b-blue-5 via-b-blue-6 to-b-blue-5 text-b-blue-6 hover:from-b-blue-4 hover:to-b-blue-4",
-    green: "bg-gradient-to-r from-b-green-5 via-b-green-6 to-b-green-5 text-b-green-6 hover:from-b-green-4 hover:to-b-green-4",
-    yellow: "bg-gradient-to-r from-b-yellow-4/90 via-b-yellow-5 to-b-yellow-4/90 text-b-yellow-6 hover:from-b-yellow-2 hover:to-b-yellow-2",
-    red: "bg-gradient-to-r from-b-red-3/90 via-b-red-5 to-b-red-3/90 text-b-red-6 hover:from-b-red-2 hover:to-b-red-2",
+    gray: "bg-gradient-to-r from-b-gray-5/30 via-b-gray-3 to-b-gray-5/50 text-b-gray-6 hover:from-b-gray-4/70 hover:to-b-gray-4/50 rounded-sm shadow-lg",
+    blue: "bg-gradient-to-r from-b-blue-5 via-b-blue-6 to-b-blue-5 text-b-blue-6 hover:from-b-blue-4 hover:to-b-blue-4 rounded-sm shadow-lg",
+    green: "bg-gradient-to-r from-b-green-5 via-b-green-6 to-b-green-5 text-b-green-6 hover:from-b-green-4 hover:to-b-green-4 rounded-sm shadow-lg",
+    yellow: "bg-gradient-to-r from-b-yellow-4/90 via-b-yellow-5 to-b-yellow-4/90 text-b-yellow-6 hover:from-b-yellow-2 hover:to-b-yellow-2 rounded-sm shadow-lg",
+    red: "bg-gradient-to-r from-b-red-3/90 via-b-red-5 to-b-red-3/90 text-b-red-6 hover:from-b-red-2 hover:to-b-red-2 rounded-sm shadow-lg",
+}
+const treeDMap: Record<string, string> = {
+    gray: `bg-b-gray-3 transition-all rounded-lg border-b-[6px] border-b-gray-4 active:border-b-0 active:translate-y-1.5 hover:brightness-110 shadow-lg`,
+    blue: `bg-b-blue-3 transition-all rounded-lg border-b-[6px] border-b-blue-4 active:border-b-0 active:translate-y-1.5 hover:brightness-110 shadow-lg`,
+    green: `bg-b-green-3 transition-all rounded-lg border-b-[6px] border-b-green-4 active:border-b-0 active:translate-y-1.5 hover:brightness-110 shadow-lg`,
+    yellow: `bg-b-yellow-4 transition-all rounded-lg border-b-[6px] border-b-yellow-5 active:border-b-0 active:translate-y-1.5 hover:brightness-110 shadow-lg`,
+    red: `bg-b-red-4 transition-all rounded-lg border-b-[6px] border-b-red-6 active:border-b-0 active:translate-y-1.5 hover:brightness-110 rounded-sm shadow-lg`,
+}
+const glossyMap: Record<string, string> = {
+    gray: `
+    rounded-full font-semibold text-white
+    bg-linear-to-b from-gray-400 via-gray-500 to-gray-600
+    shadow-[inset_0_2px_4px_rgba(255,255,255,0.4),0_4px_8px_rgba(0,0,0,0.2)]
+    border-t border-white/30
+    hover:brightness-110 active:scale-95 transition-all
+    `,
+    blue: `
+    rounded-full font-semibold text-white
+    bg-linear-to-b from-b-blue-6 via-b-blue-5 to-b-blue-4
+    shadow-[inset_0_2px_4px_rgba(255,255,255,0.4),0_4px_8px_rgba(0,0,0,0.2)]
+    border-t border-white/30
+    hover:brightness-110 active:scale-95 transition-all
+    `,
+    green: `
+    rounded-full font-semibold text-white
+    bg-linear-to-b from-b-green-6 via-b-green-5 to-b-green-4
+    shadow-[inset_0_2px_4px_rgba(255,255,255,0.4),0_4px_8px_rgba(0,0,0,0.2)]
+    border-t border-white/30
+    hover:brightness-110 active:scale-95 transition-all
+    `,
+    yellow: `
+    rounded-full font-semibold text-white
+    bg-linear-to-b from-b-yellow-6 via-b-yellow-5 to-b-yellow-4
+    shadow-[inset_0_2px_4px_rgba(255,255,255,0.4),0_4px_8px_rgba(0,0,0,0.2)]
+    border-t border-white/30
+    hover:brightness-110 active:scale-95 transition-all
+    `,
+    red: `
+    rounded-full font-semibold text-white
+    bg-linear-to-b from-b-red-6 via-b-red-5 to-b-red-4
+    shadow-[inset_0_2px_4px_rgba(255,255,255,0.4),0_4px_8px_rgba(0,0,0,0.2)]
+    border-t border-white/30
+    hover:brightness-110 active:scale-95 transition-all
+    `,
+}
+
+const neoMap: Record<string, string> = {
+    gray: `
+        font-bold border-2 border-b-gray-7 
+        bg-b-gray-2 hover:bg-b-gray-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
+        transition-all active:shadow-none active:translate-x-0.5 active:translate-y-0.5
+        `,
+    blue: `
+        font-bold border-2 border-b-gray-7 
+        bg-b-blue-4 hover:bg-b-blue-5 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
+        transition-all active:shadow-none active:translate-x-0.5 active:translate-y-0.5
+        `,
+    green: `
+        font-bold border-2 border-b-gray-7 
+        bg-b-green-5 hover:bg-b-green-4 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
+        transition-all active:shadow-none active:translate-x-0.5 active:translate-y-0.5
+        `,
+    yellow: `
+        font-bold border-2 border-b-gray-7 
+        bg-b-yellow-4 hover:bg-b-yellow-3 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] 
+        transition-all active:shadow-none active:translate-x-0.5 active:translate-y-0.5
+        `,
+    red: `
+        font-bold border-2 border-b-gray-7 
+        bg-b-red-5 hover:bg-b-red-4 shadow-[4px_4px_0px_0px_rgba(0,0,0,0.8)] 
+        transition-all active:shadow-none active:translate-x-0.5 active:translate-y-0.5
+        `,
 }
 
 const sizeMap: Record<string, string> = {
@@ -25,21 +100,38 @@ const sizeMap: Record<string, string> = {
 
 
 
-const BButton = ({ color, children, size }: BButtonProps) => {
+const BButton = ({ color, children, size, mode, onClick }: BButtonProps) => {
     return (
         <div className='w-full'>
-            <div
+            <button
+                onClick={onClick}
                 className={`
                 flex flex-row gap-2 justify-center items-center
-                ${colorMap[color]}
-                shadow-lg
+                
+                ${mode == '3d' ?
+                        treeDMap[color]
+                        :
+                        mode == 'glossy' ?
+                            glossyMap[color]
+                            :
+                            mode == 'neo' ?
+                                neoMap[color]
+                                :
+                                colorMap[color]
+                    }
+
+
+                
                 ${sizeMap[size]}
                 w-full
-                rounded-sm
+                transition-all duration-300 hover:scale-102
                 cursor-pointer
+                
             `}>
                 {children}
-            </div>
+            </button>
+
+
         </div>
     )
 }
