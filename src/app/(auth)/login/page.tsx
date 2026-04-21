44c5f92c225cb220c0e8700d168e71dfbe11d1d0
lowhanfish <lowhanfish@gmail.com>
2026-04-21 23:01:28 +0800

    Added login background

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,10 +1,30 @@
+import Image from 'next/image'
 import React from 'react'
+
 
 const page = () => {
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <main className="relative h-screen w-full">
+            <Image
+                src="/images/bg.png"
+                alt='Background'
+                fill
+                priority
+                className='object-cover z-0'
+            />
+            <div className='relative z-10 grid grid-cols-12'>
+                <div className='col-span-6'>p</div>
+                <div className='col-span-6'>
+                    <div className='p-5 md:p-10'>
+                        <div className='bg-amber-400'>
+
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </main>
     )
 }
 
