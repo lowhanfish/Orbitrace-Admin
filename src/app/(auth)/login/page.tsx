d6d42d368940927703739aafbd8c6eaeee99a2e5
lowhanfish <lowhanfish@gmail.com>
2026-04-22 18:28:45 +0800

    Added login form

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -14,21 +14,21 @@ const page = () => {
             <div className='relative h-full w-full border-2 border-b-gray-1/50 rounded-[20]'>
 
                 <Image
-                    src="/images/bg.png"
+                    src="/images/bg1.webp"
                     alt='Background'
                     priority
                     fill
                     className='object-cover z-0 rounded-[20] shadow-lg opacity-90'
                 />
-                <div className='relative z-10 grid grid-cols-12 h-full'>
-                    <div className='col-span-12 md:col-span-6'>
+                <div className='relative z-10 grid grid-cols-12 h-full justify-center items-center md:justify-normal md:items-stretch'>
+                    <div className='col-span-6 hidden md:block'>
 
 
 
                     </div>
-                    <div className='col-span-12 md:col-span-6'>
-                        <div className='h-full'>
-                            <div className='bg-white/10 md:px-30 md:py-15 backdrop-blur-sm border-l-2 border-b-gray-1/50 h-full flex flex-col gap-1 rounded-r-[20]'>
+                    <div className='col-span-12 md:col-span-6 p-5 md:p-0'>
+                        <div className='md:h-full'>
+                            <div className='bg-white/10 p-5 md:px-30 md:py-15 backdrop-blur-sm md:border-l-2 border-b-gray-1/50 h-full flex flex-col gap-1 md:rounded-r-[20]'>
 
                                 <div className='bg-white/20 px-10 py-10 md:px-20 md:py-10 backdrop-blur-sm border-2 border-b-gray-1/50 h-full flex flex-col gap-1 rounded-[10] shadow-lg'>
 
@@ -36,32 +36,22 @@ const page = () => {
                                         <Image
                                             src="/images/icon_light.png"
                                             alt='Logo'
-                                            width={200}
+                                            width={300}
                                             height={100}
                                             className='object-cover'
 
                                         />
                                     </div>
-                                    <div>
-                                        <BInput
-                                            title='Username'
-                                            placeholder='Username'
-                                            type='text'
-                                            value={textx}
-                                            onChange={(value) => {
-                                                setTextx(value)
-                                            }}
+                                    <div className='pt-5 md:pt-10'>
+                                        <p className='text-white text-[12px]'>Username</p>
+                                        <input type="text"
+                                            className='bg-white/90 h-10 w-full rounded-sm px-2 border-2 border-b-blue-3'
                                         />
                                     </div>
-                                    <div>
-                                        <BInput
-                                            title='Password'
-                                            placeholder='Password'
-                                            type='text'
-                                            value={textx}
-                                            onChange={(value) => {
-                                                setTextx(value)
-                                            }}
+                                    <div className=''>
+                                        <p className='text-white text-[12px]'>Password</p>
+                                        <input type="password"
+                                            className='bg-white/90 h-10 w-full rounded-sm px-2 border-2 border-b-blue-3'
                                         />
                                     </div>
                                 </div>
